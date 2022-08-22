c3b41d9e7e23d95483c450e6adea1eee3171a900
unknown <clemcarrig90@hotmail.com>
2022-08-22 00:09:47 -0700

    testing update

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,21 +1,19 @@
-import React from "react";
-import { render, cleanup } from "@testing-library/react";
+// __tests__/About.test.js
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import About from "..";
+import About from '..';
 
 afterEach(cleanup);
 
-describe('About component', () => {
+describe('About component renders', () => {
+  it('renders', () => {
+    render(<About />);
+  });
+  
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<About />);
     
-    it('renders', () => {
-        render(<About />);
-    });
-
-    it('matches snapshot DOM node structure', () => {
-        const { asFragment } = render(<About />);
-        expect(asFragment()).toMatchSnapshot();
-    });
-
-
+    expect(asFragment()).toMatchSnapshot();
+  });
 })
-
